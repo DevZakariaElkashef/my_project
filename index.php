76437b004c0cf4f25e7d1c53a1cc492900758772
Zakaria Elkashef <114102125+DevZakariaElkashef@users.noreply.github.com>
2023-05-18 21:39:13 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,4 +4,8 @@
 <h1>Welcome to index page</h1>
 
 <a href="./home.php">Home Page</a>
+
+
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex odit delectus dicta consequatur laborum quasi corporis et. Tempore natus placeat saepe quo fugit animi delectus, neque sapiente ea incidunt possimus!
 <?php include './inc/footer.php'; ?>
