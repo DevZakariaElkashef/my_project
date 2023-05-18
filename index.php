6db1477146c69950c123a94ebc8969e1375a8f98
Zakaria <dev.zakariaelkashef@gmail.com>
2023-05-18 21:08:05 +0300

    create include folder

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Eraasoft</title>
-</head>
-<body>
-    <h1>Learn By Practice</h1>
-</body>
-</html>
+<?php include './inc/header.php'; ?>
+
+
+<h1>Welcome to index page</h1>
+
+<a href="./home.php">Home Page</a>
+<?php include './inc/footer.php'; ?>
