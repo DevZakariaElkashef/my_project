6c3484ffc6d7ad6474aac0d31f2b68ed56a284ec
Zakaria <dev.zakariaelkashef@gmail.com>
2023-05-18 21:37:39 +0300

    modified home.php

--- home.php
+++ home.php
@@ -4,4 +4,7 @@
 <h1>Welcome to home page</h1>
 
 <a href="./index.php">Index Page</a>
+
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex odit delectus dicta consequatur laborum quasi corporis et. Tempore natus placeat saepe quo fugit animi delectus, neque sapiente ea incidunt possimus!</p>
 <?php include './inc/footer.php'; ?>
